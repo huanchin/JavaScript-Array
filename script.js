c66d5421e97faee2aad70921e4551e0ffac9da8b
huanchin <cherry032826@gmail.com>
2023-11-11 20:39:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -186,3 +186,28 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter((mov) => mov < 0);
 console.log(withdrawals); // [ -400, -650, -130 ]
+
+console.log("----- The reduce Method -----");
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance); // 3840
+
+// using for of loop
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2); // 3840
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max); // 3000
