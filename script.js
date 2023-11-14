aa9eb12d042beb1e59c8ea8a05b3efa8df22ed7b
huanchin <cherry032826@gmail.com>
2023-11-14 19:21:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -340,3 +340,30 @@ const overalBalance2 = accounts
   .flatMap((acc) => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2); // 17840
+
+console.log("-------- sort method for Strings :mutate the original array --------");
+const owners = ["Jonas", "Zach", "Adam", "Martha"];
+console.log(owners.sort()); // [ "Adam", "Jonas", "Martha", "Zach" ]
+console.log(owners); // [ "Adam", "Jonas", "Martha", "Zach" ]
+
+console.log("-------- sort method for Numbers :mutate the original array --------");
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements); // [ -650, -400, -130, 70, 200, 450, 1300, 3000 ]
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements); // [ 3000, 1300, 450, 200, 70, -130, -400, -650 ]
