6461eeb28000917be7b47268b4eba5f4bf14b21c
huanchin <cherry032826@gmail.com>
2023-11-14 19:11:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -325,7 +325,7 @@ const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(arr.flat()); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 
 const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
-console.log(arrDeep.flat(1)); // [[1, 2], 3, 4, [5, 6], 7, 8]
+console.log(arrDeep.flat()); // [[1, 2], 3, 4, [5, 6], 7, 8]
 console.log(arrDeep.flat(2)); // // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 
 // flat
