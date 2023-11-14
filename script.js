07543cfed289a1890412ded9241a60882ad3d1a5
huanchin <cherry032826@gmail.com>
2023-11-14 12:58:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -250,3 +250,21 @@ console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])); // 44
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])); // 47.333333
 // humanAges = [ 80, 40, 56, 36, 40, 2, 32 ]
 // adults = [ 80, 40, 56, 36, 40, 32 ]
+
+
+
+console.log("---- The Magic of Chaining Methods ----");
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD); // 5522.000000000001
